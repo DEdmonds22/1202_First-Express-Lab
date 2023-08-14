644db407b2313d95d3940168a18a5508e089b7b2
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-14 13:19:59 -0400

    Magic 8 Ball assignment completed

--- server.js
+++ server.js
@@ -23,7 +23,25 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
         <h2>${tipPercentage}</h2>
         <h1>Total Bill Amount: </h1>
         <h2>${totalBill}</h2>
-    `)
+    `);
+});
+
+/* Magic 8 Ball */
+const ballResponse = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+app.get("/magic", (req, res) => {
+    res.send(`
+        <h1>Write your question behind the existing URL!</h1>
+    `);
+});
+
+app.get("/magic/:question", (req, res) => {
+    const question = req.params.question.replace(/%20/g, " ");
+    const randomBallResponse = ballResponse[Math.floor(Math.random() * ballResponse.length)];
+    res.send(`
+        <h1>Your Question: ${question}?</h1>
+        <h2>Answer: ${randomBallResponse}<h2>
+    `);
 });
 
 app.listen("3000", (req, res) => {
