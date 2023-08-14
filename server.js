570fa2ffeb334bea9510953d4f6d14d465785f79
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-14 11:16:06 -0400

    Tip Calculator assignment completed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-/* GREETINGS */
 const express = require("express");
 const app = express();
 
+/* GREETINGS */
 app.get("/greeting", (req, res) => {
     res.send("<h1>Hello, Stranger</h1>");
 });
@@ -9,6 +9,21 @@ app.get("/greeting", (req, res) => {
 app.get("/greeting/:name", (req, res) => {
     const name = req.params.name;
     res.send(`<h1>Hello ${name}! How have you been?</h1>`);
+});
+
+/* TIP CALCULATOR */
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    const total = parseFloat(req.params.total);
+    const tipPercentage = `${req.params.tipPercentage}%`;
+    const totalBill = total + (total * parseFloat (req.params.tipPercentage)/100);
+    res.send(`
+        <h1>Bill: </h1>
+        <h2>${total}</h2>
+        <h1>Tip Percentage: </h1>
+        <h2>${tipPercentage}</h2>
+        <h1>Total Bill Amount: </h1>
+        <h2>${totalBill}</h2>
+    `)
 });
 
 app.listen("3000", (req, res) => {
